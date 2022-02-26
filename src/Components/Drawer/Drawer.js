f83460641ac696e3bf2ca18189346b71b11e271a
RalphSaridar55 <ralphsaridar@hotmail.com>
2022-02-26 11:55:09 +0200

    Fixed the navbar drawer

--- src/Components/Drawer/Drawer.js
+++ src/Components/Drawer/Drawer.js
@@ -9,7 +9,7 @@ const Drawer = ({ open }) =>{
           style={{ 
             //   margin:open ? "30px" : "0px",
               // padding: open? "0px 20px" : "0px",
-              display: open? "flex":'none' }}>
+              opacity: open? 1:0 }}>
           <a href="/" style={{ color: 'white' }}>Whitelist</a>
           <a href="/dashboard" style={{ color: 'white' }}>Dashboard</a>
           <a href="/" style={{ color: 'white' }}>Stake</a>
